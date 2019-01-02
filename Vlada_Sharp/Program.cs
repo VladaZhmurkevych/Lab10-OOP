2d9875cec727d83b56529efacc6b8e21587a4e15
VladaZhmurkevych <31669221+VladaZhmurkevych@users.noreply.github.com>
2019-01-02 21:35:09 +0200

    Update Program.cs

--- Vlada_Sharp/Program.cs
+++ Vlada_Sharp/Program.cs
@@ -20,12 +20,12 @@ namespace Vlada_Sharp
                 new Examination(3, "OOP", "Lischuck K.I.", 96, "Def", new DateTime(2019, 01, 09)),
                 new Examination(1, "Math", "Bodnarchuk K.K.", 100, "Def", new DateTime(2018, 01, 12)),
                 new Examination(2, "DS", "Shymcovych V.I.", 86, "Def", new DateTime(2018, 06, 13)),
-                new Examination(4, "WEB", "Isachenko G.L.", 75, "Def", new DateTime(2019, 06, 12)),
+                new Examination(4, "Cryptography", "Isachenko G.L.", 75, "Def", new DateTime(2019, 06, 12)),
             });
             Console.WriteLine(edward.ToString());
             edward.PrintFullInfo();
 
-            Console.WriteLine("Exersive 1(2): ");
+            Console.WriteLine("Exercise 1(2): ");
             Person p1 = new Person("Robert", "Wilson", new DateTime(1999, 11, 18));
             Person p2 = new Person("Robert", "Wilson", new DateTime(1999, 11, 18));
             Person p3 = new Person("John", "Smith", new DateTime(1998, 10, 12));
@@ -35,7 +35,7 @@ namespace Vlada_Sharp
             Console.WriteLine("Robert Wilson 18.11.1999 != John Smith 12.10.1998 : " + (p1 != p3));
             Console.WriteLine();
 
-            Console.WriteLine("Exersive 2(3): ");
+            Console.WriteLine("Exercise 2(3): ");
             Examination exam = new Examination();
             exam.Mark = 87;
             Console.WriteLine("Mark 87");
@@ -43,7 +43,7 @@ namespace Vlada_Sharp
             Console.WriteLine("In ECTS scale: {0}", exam.EctsScaleName());
             Console.WriteLine();
 
-            Console.WriteLine("Exersive 3(5): ");
+            Console.WriteLine("Exercise 3(5): ");
             Console.WriteLine("Exams with mark more than 85: ");
              foreach (Examination exm in edward.GetExaminations(86))
              {
@@ -51,7 +51,7 @@ namespace Vlada_Sharp
              }
             Console.WriteLine();
 
-            Console.WriteLine("Exersive 4(10): ");
+            Console.WriteLine("Exercise 4(10): ");
             Console.WriteLine("Exams sorted by semester: ");
             edward.GetCredits();
 
